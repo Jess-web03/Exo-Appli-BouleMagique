f86f8a482fddac98c33dd5070289e7037af02da4
Jessica <jes.meerts@gmail.com>
2020-03-16 21:15:23 +0100

    loader qui marche

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-function myFunctionLoad() {
-  //alert('en cours');
-}
-myFunctionLoad();
+document.getElementById('myBtnReset').addEventListener('click', () => {
+  console.log('je clique encore');
+  document.getElementById('phrase').innerHTML = '';
+});
 
+document.getElementById('myBtn').addEventListener('click', () => {
+  console.log('je clique');
 
-function func() {
+  document.getElementById('loader').style.display = 'block';
+  document.getElementById('loader1').style.display = 'block';
+
   setTimeout(() => {
-    let prevu = '';
+    let prevu;
     if ((Math.floor(Math.random() * 10) + 1) === 0) {
       prevu = "pas pour l'instant";
     } else if ((Math.floor(Math.random() * 10) + 1) === 1) {
@@ -29,15 +33,57 @@ function func() {
       prevu = 'Un peu de patience';
     } else if ((Math.floor(Math.random() * 10) + 1) === 10) {
       prevu = 'Demande à un autre voyant';
+    } else {
+      prevu = 'Fort probable ... ou pas, c\'était une blague';
     }
-
-
-    const prevoir = document.createElement('p');
-    const textprevoir = document.createTextNode(prevu);
-    prevoir.appendChild(textprevoir);
     document.getElementById('loader').style.display = 'none';
     document.getElementById('loader1').style.display = 'none';
-    document.getElementById('bouleMagic').appendChild(prevoir);
+    document.getElementById('phrase').innerHTML = prevu;
   }, 2000);
-}
-func();
+});
+
+// function myFunctionLoad() {
+//   //alert('en cours');
+// }
+// myFunctionLoad();
+
+
+// function func() {
+
+//   console.log('test');
+//   setTimeout(() => {
+//     let prevu = '';
+//     if ((Math.floor(Math.random() * 10) + 1) === 0) {
+//       prevu = "pas pour l'instant";
+//     } else if ((Math.floor(Math.random() * 10) + 1) === 1) {
+//       prevu = 'Bientôt';
+//     } else if ((Math.floor(Math.random() * 10) + 1) === 2) {
+//       prevu = "c'est possible, mais n'espérez pas trop";
+//     } else if ((Math.floor(Math.random() * 10) + 1) === 3) {
+//       prevu = "L'année prochaine";
+//     } else if ((Math.floor(Math.random() * 10) + 1) === 4) {
+//       prevu = "c'est certain !";
+//     } else if ((Math.floor(Math.random() * 10) + 1) === 5) {
+//       prevu = 'Laisse tomber, impossible';
+//     } else if ((Math.floor(Math.random() * 10) + 1) === 6) {
+//       prevu = "N'espère pas trop";
+//     } else if ((Math.floor(Math.random() * 10) + 1) === 7) {
+//       prevu = "L'année prochaine";
+//     } else if ((Math.floor(Math.random() * 10) + 1) === 8) {
+//       prevu = 'Presque, occasion ratée';
+//     } else if ((Math.floor(Math.random() * 10) + 1) === 9) {
+//       prevu = 'Un peu de patience';
+//     } else if ((Math.floor(Math.random() * 10) + 1) === 10) {
+//       prevu = 'Demande à un autre voyant';
+//     }
+
+
+//     const prevoir = document.createElement('p');
+//     const textprevoir = document.createTextNode(prevu);
+//     prevoir.appendChild(textprevoir);
+//     document.getElementById('loader').style.display = 'none';
+//     document.getElementById('loader1').style.display = 'none';
+//     document.getElementById('bouleMagic').appendChild(prevoir);
+//   }, 2000);
+// }
+// func();
